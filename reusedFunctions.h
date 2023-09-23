36948a744fae37334427f265486ab84324ea2004
arpit goswami <87869007+danzra@users.noreply.github.com>
2023-09-23 19:44:09 +0530

    Add files via upload

---
+++ reusedFunctions.h
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+// Supporting-Functions
+void spaces1()
+{
+    for (int i = 0; i < 4; i++)
+        cout << endl;
+    cout << "\t\t\t\t\t BANK OF INDIA (BOI) " << endl;
+}
+void spaces2()
+{
+    for (int i = 0; i < 2; i++)
+        cout << endl;
+}
